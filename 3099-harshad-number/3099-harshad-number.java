0093fcffef386c2095859ad128e4e527640a370f
Nrusingh prasad swain <132093255+nrusingh-07@users.noreply.github.com>
2026-03-28 20:29:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (73.9%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int div = x;
+        int sum = 0;
+        while(x > 0) {
+            int digit = x % 10;
+            sum += digit;
+            x = x/10;
+        }
+        if(div % sum == 0) {
+            return sum;
+        }
+        else{
+            return -1;
+        }
+    }
+}
